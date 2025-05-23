8b8fd2fd31eee10b0cc2e54f425cc2e1a34d681e
Denis Badin <denisbadin@yandex.ru>
2025-05-23 16:32:33 +0500

    все зафиксировал

--- src/widget.py
+++ src/widget.py
@@ -1,22 +1,26 @@
 from typing import Union
-from masks import det_masks_card_number
-from masks import get_masks_account
+
+from masks import get_masks_account, det_masks_card_number
+
 
 def mask_account_card(account_card: Union[str]) -> Union[str]:
+    """Функция маскировки счета или карты"""
     num = ""
     nam = ""
     for i in account_card:
         if i.isdigit():
             num += i
         elif i.isalpha():
-            nam +=i
+            nam += i
     if nam == "Счет":
-        return nam + " " + get_masks_account(num)
+        return f"{nam} {get_masks_account(num)}"
     else:
-        return nam + " " + det_masks_card_number(num)
+        return f"{nam} {det_masks_card_number(num)}"
 
-def get_date (account_date: Union[str]) -> Union[str]:
-    dat_acc = ''
+
+def get_date(account_date: Union[str]) -> Union[str]:
+    """Функция преобразует входящуюю дату в формат хх.хх.хххх"""
+    dat_acc = ""
     for i_d in range(10):
         dat_acc += account_date[i_d]
     new_dat_acc = dat_acc.split("-")
@@ -24,18 +28,13 @@ def get_date (account_date: Union[str]) -> Union[str]:
     return ".".join(new_dat_acc)
 
 
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
 
-
-# print (mask_account_card('Maestro 1596837868705199'))
-# print (mask_account_card('Счет 64686473678894779589'))
-# print (mask_account_card('MasterCard 7158300734726758'))
-# print (mask_account_card('Счет 35383033474447895560'))
-# print (mask_account_card('Visa Classic 6831982476737658'))
-# print (mask_account_card('Visa Platinum 8990922113665229'))
-# print (mask_account_card('Visa Gold 5999414228426353'))
-# print (mask_account_card('Счет 73654108430135874305'))
-#
-# print (get_date('2024-03-11T02:26:18.671407'))
-
-
-
+print(get_date("2024-03-11T02:26:18.671407"))
