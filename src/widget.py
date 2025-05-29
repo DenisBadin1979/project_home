9d27253f58225f9f7e10d3bf30a7d8eee8f7cc91
Denis_Badin <denisbadin@yandex.ru>
2025-05-29 22:34:54 +0500

    изменеия на домашнем

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from masks import get_masks_account, det_masks_card_number
+from src.masks import get_masks_account, det_masks_card_number
 
 
 def mask_account_card(account_card: Union[str]) -> Union[str]:
@@ -26,15 +26,3 @@ def get_date(account_date: Union[str]) -> Union[str]:
     new_dat_acc = dat_acc.split("-")
     new_dat_acc.reverse()
     return ".".join(new_dat_acc)
-
-
-print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
-
-print(get_date("2024-03-11T02:26:18.671407"))
