4a1af983af5c08f5cc9c09807a7c0748a76d7706
Denis_Badin <denisbadin@yandex.ru>
2025-05-31 19:06:51 +0500

    new function

--- src/processing.py
+++ src/processing.py
@@ -1,24 +1,17 @@
-
-
-
-def filter_by_state(operation, stat ="EXECUTED"):
+def filter_by_state(operation: list[dict], stat: str="EXECUTED") -> list[dict]:
     """Функция принимает список словарей и опционально значение для ключа
-state и возвращает новый список словарей, содержащий только те словари, у которых ключ
-state
- соответствует указанному значению."""
+    state и возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state
+     соответствует указанному значению."""
     filter_result = []
     for operations in operation:
-        if operations['state'] == stat:
+        if operations["state"] == stat:
             filter_result.append(operations)
     return filter_result
 
 
-def sort_by_date (operation, revers = True):
+def sort_by_date(operation: list[dict], revers: bool=True)-> list[dict]:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание).  должна возвращать новый список"""
-    sorted_date = sorted(operation, key=lambda operations: operations['date'],reverse=revers)
+    sorted_date = sorted(operation, key=lambda operations: operations["date"], reverse=revers)
     return sorted_date
-
-
-
-
