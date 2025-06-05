15dedbde68ff5be59b7faa3f597231970c5a3734
Denis_Badin <denisbadin@yandex.ru>
2025-06-06 02:50:50 +0500

    все изменния

--- src/widget.py
+++ src/widget.py
@@ -12,12 +12,24 @@ def mask_account_card(account_card: Union[str]) -> Union[str]:
             num += i
         elif i.isalpha():
             nam += i
-    if nam == "Счет":
+    if nam == "Счет" and len(num) == 20:
         return f"{nam} {get_masks_account(num)}"
-    else:
+    elif len(num) == 16 and account_card[-16:].isdigit():
+        # type_card = account_card[:-16]
+        # number_card = account_card[-16:]
         return f"{nam} {det_masks_card_number(num)}"
+    else:
+        return "неверно указан номер"
 
 
+# def get_date(account_date: Union[str]) -> Union[str]:
+#     """Функция преобразует входящуюю дату в формат хх.хх.хххх"""
+#     dat_acc = ""
+#     for i_d in range(10):
+#         dat_acc += account_date[i_d]
+#     new_dat_acc = dat_acc.split("-")
+#     new_dat_acc.reverse()
+#     return ".".join(new_dat_acc)
 
 
 def get_date(date_time: str) -> str:
@@ -25,5 +37,9 @@ def get_date(date_time: str) -> str:
     Функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ"
     """
-    times = date_time[:date_time.find("T")].split("-")
-    return ".".join(times)
+    if "T" in date_time and len(date_time) > 11:
+        times = date_time[: date_time.find("T")].split("-")
+        times.reverse()
+        return ".".join(times)
+    else:
+        return "Неверный формат даты"
