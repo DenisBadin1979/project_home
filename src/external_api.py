74b265d0b0037f72d66a92cb7a35b709baec3aa7
Denis_Badin <denisbadin@yandex.ru>
2025-06-30 05:04:59 +0500

    bible

--- src/external_api.py
+++ src/external_api.py
@@ -1,23 +1,16 @@
-import requests
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
 def currency_converter(currency: str, amount: str) -> float:
+    """Функцию конвертации валюты"""
     url = "https://api.apilayer.com/exchangerates_data/convert"
-    payload = {
-        "amount": amount,
-        "from": currency,
-        "to": "RUB"}
-    api_key_d = os.getenv('API_KEY')
+    payload = {"amount": amount, "from": currency, "to": "RUB"}
+    api_key_d = os.getenv("API_KEY")
     headers = {"apikey": api_key_d}
     response = requests.get(url, headers=headers, params=payload)
     result_convert = response.json()
-    return result_convert['result']
-
-
-
-
-
-
-
+    return result_convert["result"]
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,14 @@
 import json
+
 from src.external_api import currency_converter
 
 
-
-def operations_json (file_path: str) -> list[dict]:
+def operations_json(file_path: str) -> list[dict]:
+    """функция,  принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
-        with open(file_path , "r", encoding='utf-8') as f:
+        with open(file_path, "r", encoding="utf-8") as f:
             return json.load(f)
     except json.JSONDecodeError:
         return []
     except FileNotFoundError:
         return []
-
-
-
-
-
-
